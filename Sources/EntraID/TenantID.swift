2e6dce268eaab9f14b056a03f8a0b34913088f76
Alsey Coleman Miller <alseycmiller@gmail.com>
2025-10-01 12:58:47 -0400

    Add `TenantID`

---
+++ Sources/EntraID/TenantID.swift
@@ -0,0 +1,54 @@
+//
+//  TenantID.swift
+//  EntraID
+//
+//  Created by Alsey Coleman Miller on 10/1/25.
+//
+
+import Foundation
+
+/// Microsoft Entra Tenant ID
+public struct TenantID: Codable, Equatable, Hashable, Sendable {
+    
+    internal let uuid: UUID
+    
+    public init(_ uuid: UUID) {
+        self.uuid = uuid
+    }
+}
+
+public extension UUID {
+    
+    init(_ id: TenantID) {
+        self = id.uuid
+    }
+}
+
+// MARK: - CustomStringConvertible
+
+extension TenantID: RawRepresentable {
+    
+    public init?(rawValue: String) {
+        guard let uuid = UUID(uuidString: rawValue) else {
+            return nil
+        }
+        self.init(uuid)
+    }
+    
+    public var rawValue: String {
+        uuid.uuidString.lowercased()
+    }
+}
+
+// MARK: - CustomStringConvertible
+
+extension TenantID: CustomStringConvertible, CustomDebugStringConvertible {
+    
+    public var description: String {
+        rawValue
+    }
+    
+    public var debugDescription: String {
+        rawValue
+    }
+}
