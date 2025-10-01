4c16bedc4120ffb8327698807758129a905d3b5c
Alsey Coleman Miller <alseycmiller@gmail.com>
2025-10-01 14:10:08 -0400

    Add `URLClient`

---
+++ Sources/EntraID/URLClient.swift
@@ -0,0 +1,21 @@
+//
+//  URLClient.swift
+//  EntraID
+//
+//  Created by Alsey Coleman Miller on 10/1/25.
+//
+
+import Foundation
+#if canImport(FoundationNetworking)
+@_exported import FoundationNetworking
+#endif
+
+/// URL Client
+public protocol URLClient {
+    
+    associatedtype URLError: Error
+    
+    func data(
+        for request: URLRequest
+    ) async throws(URLError) -> (Data, URLResponse)
+}
