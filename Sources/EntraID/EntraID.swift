5b01dcb48563ad5ff2f2f6d43c46c50a65e7f23d
Alsey Coleman Miller <alseycmiller@gmail.com>
2025-10-01 12:59:37 -0400

    Add umbrella import

---
+++ Sources/EntraID/EntraID.swift
@@ -0,0 +1,15 @@
+//
+//  TenantID.swift
+//  EntraID
+//
+//  Created by Alsey Coleman Miller on 10/1/25.
+//
+
+import Foundation
+#if canImport(FoundationNetworking)
+import FoundationNetworking
+#endif
+import HTTPTypes
+#if canImport(HTTPTypesFoundation)
+import HTTPTypesFoundation
+#endif
